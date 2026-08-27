16a2f95c0301c8e072507c66eae3f235b8e5fe86
devrickdev <shimmoo2001@gmail.com>
2026-08-27 17:38:06 -0300

    primeiro commit do Fixtech

---
+++ src/main/java/com/mycompany/fixtech/Fixtech.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fixtech;
+
+/**
+ *
+ * @author henry
+ */
+public class Fixtech {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
