06b964e8d5a3b0d6ecbca6ea74000c0d0217ffd1
foiofranco <joaofsfranco@gmail.com>
2026-09-03 14:56:14 -0300

    Segundo Commit - FixTech

--- src/main/java/com/mycompany/fixtech/Fixtech.java
+++ src/main/java/com/mycompany/fixtech/Fixtech.java
@@ -11,6 +11,6 @@ package com.mycompany.fixtech;
 public class Fixtech {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
     }
 }
